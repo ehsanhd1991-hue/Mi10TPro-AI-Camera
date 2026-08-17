17d2dd618c4a61cb36b0fac388d77388ef2c1fee
ehsanhd1991-hue <ehsan.hd1991@gmail.com>
2026-08-17 22:18:00 +0330

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mi10tpro.aicamera"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mi10tpro.aicamera"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+
+    implementation("androidx.camera:camera-core:1.4.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
+}
